62e91f6d604b56a80cbd039f1295a38c160e46e9
Souvik Guchait <souvikguchait64@gmail.com>
2026-07-20 04:23:24 +0530

    Initial commit

---
+++ ML/D61_Numpy/Numpy_basics ii/Arithmatics.py
@@ -0,0 +1,17 @@
+# ======================================
+#  # TASK 7: Element-wise Operations
+#  # ====================================== 
+import numpy as np
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+
+ # Output: 
+ # # [5 7 9] 
+ # # [-3 -3 -3]
+ #  # [ 4 10 18]
+ #  # [0.25 0.4 0.5 ]
---
+++ ML/D61_Numpy/Numpy_basics ii/multidiamention_to_1D.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+])
+
+print(arr.flatten())  
+
+#output
+# [1 2 3 4 5 6]  here multidiamentiuon array is converted to 1D array using flatten() function
