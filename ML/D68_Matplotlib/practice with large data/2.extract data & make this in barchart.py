10617e718e94c38deace35fe9e7550623c5a08d0
Souvik Guchait <souvikguchait64@gmail.com>
2026-07-28 03:41:43 +0530

    Initial Commit

---
+++ ML/D68_Matplotlib/practice with large data/2.extract data & make this in barchart.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+url=" https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/021/299/original/final_vg1_-_final_vg_%281%29.csv?1670840166 "
+df = pd.read_csv(url)
+df.to_csv("final_vg.csv",index=False)
+print(df["Genre"])
+print(df["Genre"].unique()) # unique value from genre colume
+cat_counts=df["Genre"].value_counts() # its show count genre
+print(cat_counts)
+x=cat_counts.index
+y=cat_counts.values
+plt.figure(figsize=(12,8)) # make large size of figure so not to be overlap
+# plt.plot(x,y)
+# plt.savefig("1.Genre Data in linechart.png",dpi=350,bbox_inches='tight') #save result with resolution accrding me 
+
+plt.bar(x,y)
+plt.savefig("2a Genre Data in barchart.png",dpi=350,bbox_inches='tight') #save result with resolution accrding me 
+plt.xticks(rotation=90,fontsize=10)
+plt.savefig("2b Genre Data in barchart.png",dpi=350,bbox_inches='tight')
