bbc5ff26d3de28c773e7ce3bea92724a591b0a9e
Souvik Guchait <souvikguchait64@gmail.com>
2026-08-28 22:56:23 +0530

    Add Flask app

---
+++ Flask/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,redirect,url_for,session,Response
+app=Flask(__name__)
+app.secret_key = "supersecret"
+
+#homepage login page
+@app.route("/",methods=["GET","POST"])
+
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "123":
+            session["user"] = username #store in session
+            return redirect(url_for("welcome"))
+        else:
+            return Response("In-valid credentials. Try again",mimetype="text/plain")
+
+    return '''
+           
+        <h2>Login Page</h2>
+
+        <form method="POST">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+
+
+
+'''    
